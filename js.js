e5c23c777a832f2066b703ea9fc4689b36c0de63
omarelansary <73857229+omarelansary@users.noreply.github.com>
2023-04-26 14:55:25 +0200

    missing contact us social media
    missing comments form ali about
    1. jot form
    2. background images
    3. About section
    4. video
    5. typewritten text

--- js.js
+++ js.js
@@ -1,44 +1,66 @@
-'use strict';
+"use strict";
 
-var sloganElementlg=document.getElementById("sloganlg");
-var sloganElement=document.getElementById("slogan");
+var sloganElementlg = document.getElementById("sloganlg");
+var sloganElement = document.getElementById("slogan");
 
 // Tabbed Menu
 function openMenu(evt, menuName) {
-var i, x, tablinks;
-x = document.getElementsByClassName("menu");
-for (i = 0; i < x.length; i++) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("menu");
+  for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
-}
-tablinks = document.getElementsByClassName("tablink");
-for (i = 0; i < x.length; i++) {
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+  }
+  document.getElementById(menuName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " w3-red";
 }
-document.getElementById(menuName).style.display = "block";
-evt.currentTarget.firstElementChild.className += " w3-red";
+
+var increment,
+  incrementlg = 0;
+var text = "Del Gusto Italiano";
+function sloganText() {
+  for (var i = 0; i < text.length; i++) {
+    sloganElement.textContent += text[i];
+  }
 }
+// function sloganText() {
+//   setTimeout(function () {
+//     for (var i = 0; i < text.length; i++) {
+//       sloganElement.textContent += text[i];
+//     }
+//   }, 200);
 
-var increment,incrementlg=0;
-var text="Del Gusto Italiano"
+//   //sloganElementlg.textContent+=text[i];
+// }
+// function typeWriter() {
+//   if (increment < text.length) {
+//     sloganElement.innerHTML += text.charAt(increment);
+//     increment++;
+//     setTimeout(typeWriter, 200);
+//   }
+// }
 
-setInterval(function(){
-    if (increment<text.length){
-        sloganElement.textContent+=text[increment];
-        increment++;
-    }
-    else
-    {increment=0;
-     sloganElement.textContent="";}
-}, 200);
+// setInterval(function(){
+//     if (increment<text.length){
+//         sloganElement.textContent+=text[increment];
+//         increment++;
+//     }
+//     else
+//     {increment=0;
+//      sloganElement.textContent="";}
+// }, 200);
 
-setInterval(function(){
-    if (incrementlg<text.length){
-        sloganElementlg.textContent+=text[incrementlg];
-        incrementlg++;
-    }
-    else
-    {incrementlg=0;
-     sloganElementlg.textContent="";}
-}, 200);
+// setInterval(function(){
+//     if (incrementlg<text.length){
+//         sloganElementlg.textContent+=text[incrementlg];
+//         incrementlg++;
+//     }
+//     else
+//     {incrementlg=0;
+//      sloganElementlg.textContent="";}
+// }, 200);
 
-document.getElementById("myLink").click();
+//document.getElementById("myLink").click();
